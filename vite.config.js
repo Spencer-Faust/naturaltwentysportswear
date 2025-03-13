9d6fc5b4932f424927b01ee1ad23b31e3da8ca7b
Spencer <spencer.faust@kineticsdata.com>
2025-03-13 12:51:44 -0500

    Config update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/naturaltwentysportswear/',
   plugins: [react()],
   optimizeDeps: { exclude: ["fsevents"] },
-  build: {
-    base: '/naturaltwentysportswear/',
-  },
 })
